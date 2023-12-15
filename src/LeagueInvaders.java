695a331d04dfbe90e4bfb2c3153d433445a2a1a6
league <league@imac-18.attlocal.net>
2023-12-14 17:01:11 -0800

    Started workign on League Invaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,17 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	public LeagueInvaders() {
+		frame = new JFrame();
+	}
+	public static void main (String[] args) {
+		leagueGame = new LeagueInvaders();
+	}
+	public void setup() {
+		frame.setSize(HEIGHT, WIDTH);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
